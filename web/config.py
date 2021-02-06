b4e71db97bd3f1a9b1f89f1c0455cd38f15f9a0b
Caglan <cakpinar23@gmail.com>
2021-02-06 18:53:26 +0300

    config for web

---
+++ web/config.py
@@ -0,0 +1,33 @@
+import os
+from decouple import config
+
+
+class Config(object):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SESSION_COOKIE_HTTPONLY  = True
+    REMEMBER_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_DURATION = 3600
+
+    # PostgreSQL database
+    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
+        config('DB_ENGINE', default='postgresql'),
+        config('DB_USERNAME', default='appseed'),
+        config('DB_PASS', default='pass'),
+        config('DB_HOST', default='localhost'),
+        config('DB_PORT', default=5432),
+        config('DB_NAME', default='appseed-flask')
+    )
+
+
+class DebugConfig(Config):
+    DEBUG = True
+
+
+config_dict = {'Production': ProductionConfig, 'Debug': DebugConfig}
