6e5073eae48e33d719799d875b4bcf022d5535c0
caglanakpinar <cakpinar23@gmail.com>
2021-02-06 22:31:57 +0300

    configs is storing on explotarory_analysis folder of __init__.py

--- exploratory_analysis/__init__.py
+++ exploratory_analysis/__init__.py
@@ -7,22 +7,41 @@ from exploratory_analysis.funnels import Funnels
 from exploratory_analysis.cohorts import Cohorts
 from exploratory_analysis.product_analytics import ProductAnalytics
 from exploratory_analysis.rfm import RFM
+from exploratory_analysis.descriptive_statistics import Stats
 
+
+configs = {"date": None,
+           "funnel": {"actions": ["download", "signup"],
+                      "purchase_actions": ["has_basket", "order_screen"],
+                      "host": 'localhost',
+                      "port": '9200',
+                      'download_index': 'downloads',
+                      'order_index': 'orders'},
+           "cohort": {"has_download": True, "host": 'localhost', "port": '9200'},
+           "products": {"has_download": True, "host": 'localhost', "port": '9200'},
+           "rfm": {"host": 'localhost', "port": '9200', 'download_index': 'downloads', 'order_index': 'orders'},
+           "stats": {"host": 'localhost', "port": '9200', 'download_index': 'downloads', 'order_index': 'orders'}
+          }
 
 exploratory_analysis = {'funnel': Funnels,
                         'cohort': Cohorts,
                         'products': ProductAnalytics,
-                        'rfm': RFM}
+                        'rfm': RFM,
+                        'stats': Stats}
 
 
 def create_exploratory_analysis(configs):
     ea = {a: exploratory_analysis[a](**configs[a]) for a in exploratory_analysis}
     ea['funnel'].purchase_action_funnel(start_date=configs['date'])
     ea['funnel'].download_signup_session_order_funnel(start_date=configs['date'])
-    ea['funnel'].overall_funnel(start_date=configs['date'])
+    ea['funnel'].overall_funnel(start_date=configs['date'], index=configs['funnel']['order_index'])
+    ea['cohort'].execute_cohort(start_date=configs['date'])
+    ea['rfm'].execute_rfm(start_date=configs['date'])
+    ea['stats'].execute_descriptive_stats(start_date=configs['date'])
 
 
 def query_exploratory_analysis(configs, queries, ea):
+    print(configs[ea])
     ea = exploratory_analysis[ea](**configs[ea])
     return ea.fetch(**queries)
 
