b515e10354f67bfc65c6b649aa8bdbdb9f8bac82
caglanakpinar <cakpinar23@gmail.com>
2021-02-17 21:41:49 +0300

    confisgs.py some cosmetics for PEP8

--- configs.py
+++ configs.py
@@ -94,8 +94,8 @@ elasticsearch_connection_refused_comment = """
             """
 
 default_message = {'orders': '....',
-                        'orders_data': '....',
-                        'orders_columns': '....',
-                        'downloads': '....',
-                        'downloads_data': '....',
-                        'downloads_columns': '....'}
+                   'orders_data': '....',
+                   'orders_columns': '....',
+                   'downloads': '....',
+                   'downloads_data': '....',
+                   'downloads_columns': '....'}
