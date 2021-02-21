0426cb4d1ed4e5cc58c55bcdbd300d3ac003d54a
Caglan <cakpinar23@gmail.com>
2021-02-21 23:46:40 +0300

    clv_prediction.py is created

---
+++ ml_process/clv_prediction.py
@@ -0,0 +1,159 @@
+import sys, os, inspect
+import pandas as pd
+import numpy as np
+import shutil
+from clv.executor import CLV
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from configs import default_es_port, default_es_host, default_query_date
+from utils import *
+from data_storage_configurations.query_es import QueryES
+
+
+class CLVPrediction:
+    """
+    Customer Lifetime Value Prediction;
+
+
+
+    """
+    def __init__(self,
+                 temporary_export_path,
+                 host=None,
+                 port=None,
+                 download_index='downloads',
+                 order_index='orders'):
+        """
+        ******* ******** *****
+        Dimensional Customer Segmentation:
+        Descriptive Statistics must be created individually for dimensions.
+        For instance, the Data set contains locations dimension.
+        In this case, each location of 'orders' and 'downloads' indexes must be created individually.
+        by using 'download_index' and 'order_index' dimension can be assigned in order to create the Segmentations
+
+        download_index; downloads_location1 this will be the location dimension of
+                        parameters in order to query downloads indexes; 'location1'.
+        download_index; orders_location1 this will be the location dimension of
+                        parameters in order to query orders indexes; 'location1'.
+        ******* ******** *****
+        !!!
+
+        :param host: elasticsearch host
+        :param port: elasticsearch port
+        :param download_index: elasticsearch port
+        :param order_index: elasticsearch port
+        """
+        self.port = default_es_port if port is None else port
+        self.host = default_es_host if host is None else host
+        self.download_index = download_index
+        self.order_index = order_index
+        self.clv = None
+        self.query_es = QueryES(port=port, host=host)
+        self.path = temporary_export_path
+        self.temp_csv_file = join(temporary_export_path, "temp_data.csv")
+        self.clv_fields = ["client", "session_start_date", "payment_amount"]
+        self.clv_predictions = pd.DataFrame()
+
+    def get_orders_data(self, end_date):
+        """
+
+        :return:
+        """
+        self.query_es = QueryES(port=self.port, host=self.host)
+        self.query_es.date_queries_builder({"session_start_date": {"lt": end_date}})
+        self.query_es.boolean_queries_buildier({"actions.purchased": True})
+        self.query_es.query_builder(fields=self.clv_fields)
+        pd.DataFrame(self.query_es.get_data_from_es(index=self.order_index)).to_csv(self.temp_csv_file, index=False)
+
+    def insert_into_reports_index(self, clv_predictions, time_period, date=None, index='orders'):
+        """
+        via query_es.py, each report can be inserted into the reports index with the given format.
+        {"id": unique report id,
+         "report_date": date or current date,
+         "report_name": "clv_prediction",
+         "index": "main",
+         "report_types": {"time_period": weekly, monthly, daily },
+         "data": segments.fillna(0.0).to_dict("results") -  dataframe to list of dictionary
+         }
+         !!! null values are assigned to 0.
+
+        :param segments: data set, data frame
+        :param index: dimensionality of data index orders_location1 ;  dimension = location1
+        """
+        list_of_obj = [{"id": np.random.randint(200000000),
+                        "report_date": current_date_to_day().isoformat() if date is None else date,
+                        "report_name": "clv_prediction",
+                        "index": get_index_group(index),
+                        "report_types": {"time_period": time_period},
+                        "data": clv_predictions.fillna(0.0).to_dict("results")}]
+        self.query_es.insert_data_to_index(list_of_obj, index='reports')
+
+    def convert_time_period(self, period):
+        if period == 'weekly':
+            return 'week'
+        if period == 'daily':
+            return 'day'
+        if period == 'monthly':
+            return 'month'
+
+    def execute_clv(self, start_date, job='train', time_period='weekly'):
+        start_date = str(current_date_to_day())[0:10] if start_date is None else start_date
+        self.get_orders_data(end_date=start_date)
+        self.clv = CLV(customer_indicator="client",
+                       amount_indicator="payment_amount",
+                       job=job,
+                       date=start_date,
+                       data_source='csv',
+                       data_query_path=self.temp_csv_file,
+                       time_period=self.convert_time_period(time_period),
+                       time_indicator="session_start_date",
+                       export_path=self.path)
+        self.clv.clv_prediction()
+        if job == 'train':
+            self.clv = CLV(customer_indicator="client",
+                           amount_indicator="payment_amount",
+                           job='prediction',
+                           date=start_date,
+                           data_source='csv',
+                           data_query_path=self.temp_csv_file,
+                           time_period=self.convert_time_period(time_period),
+                           time_indicator="session_start_date",
+                           export_path=self.path)
+            self.clv.clv_prediction()
+        self.clv_predictions = \
+        self.clv.get_result_data().query("data_type == 'prediction' and session_start_date == session_start_date")[
+            ['session_start_date', 'client', 'payment_amount']]
+        self.insert_into_reports_index(self.clv_predictions,
+                                       time_period=time_period,
+                                       date=start_date,
+                                       index=self.order_index)
+
+    def fetch(self, end_date=None, time_period='week'):
+        """
+        :param start_date:
+        :param time_period:
+        :return:
+        """
+
+        boolean_queries, date_queries = [], []
+        boolean_queries = [{"term": {"report_name": "clv_prediction"}},
+                           {"term": {"report_types.time_period": time_period}}]
+
+        if end_date is not None:
+            date_queries = [{"range": {"report_date": {"lt": convert_to_iso_format(end_date)}}}]
+
+        self.query_es = QueryES(port=self.port,
+                                host=self.host)
+        self.query_es.query_builder(fields=None, _source=True,
+                                    date_queries=date_queries,
+                                    boolean_queries=boolean_queries)
+        _res = self.query_es.get_data_from_es(index="reports")
+        _data = pd.DataFrame()
+        if len(_res) != 0:
+            _data = pd.DataFrame(_res[0]['_source']['data'])
+            if end_date is not None:
+                _data[time_period] = _data["session_start_date"].apply(lambda x: convert_to_date(x))
+        return _data
