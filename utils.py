0eaf97b6129354a3b8ed1b9e4a8a6530093fcda1
caglanakpinar <cakpinar23@gmail.com>
2021-03-02 19:55:31 +0300

    rgb_to_hex and hex_to_rgb are added to utils.py

--- utils.py
+++ utils.py
@@ -117,3 +117,13 @@ def convert_dt_to_month_str(date):
     """
     return datetime.datetime.strptime(str(date)[0:7], "%Y-%m")
 
+
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
