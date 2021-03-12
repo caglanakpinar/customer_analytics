fe0b97c2c8d4642be5b27ed568d1eff8427190d2
Caglan <cakpinar23@gmail.com>
2021-03-12 23:53:11 +0300

    abtest is added to __init__

--- ml_process/__init__.py
+++ ml_process/__init__.py
@@ -5,17 +5,21 @@ sys.path.insert(0, parentdir)
 
 from ml_process.customer_segmentation import CustomerSegmentation
 from ml_process.clv_prediction import CLVPrediction
+from ml_process.ab_test import ABTests
 
 configs = {"date": None,
            "time_period": "weekly",
            "segmentation": {"host": 'localhost', "port": '9200', 'download_index': 'downloads', 'order_index': 'orders'},
            "clv_prediction": {"temporary_export_path": None,
-                              "host": 'localhost', "port": '9200', 'download_index': 'downloads', 'order_index': 'orders'}
+                              "host": 'localhost', "port": '9200', 'download_index': 'downloads', 'order_index': 'orders'},
+           "abtest": {"temporary_export_path": None,
+                      "host": 'localhost', "port": '9200', 'download_index': 'downloads', 'order_index': 'orders'}
           }
 
 
 mls = {'segmentation': CustomerSegmentation,
-       'clv_prediction': CLVPrediction
+       'clv_prediction': CLVPrediction,
+       'abtest': ABTests
       }
 
 # TODO: anomaly detection for cohort
@@ -26,6 +30,7 @@ def create_mls(configs):
     ea = {a: mls[a](**configs[a]) for a in mls}
     ea['segmentation'].execute_customer_segment(start_date=configs['date'])
     ea['clv_prediction'].execute_clv(start_date=configs['date'], time_period=configs['time_period'])
+    ea['abtest'].build_in_tests(date=configs['date'])
 
 
 def query_mls(configs, queries, ea):
