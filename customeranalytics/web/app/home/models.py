a2fa5e375c3bc1086f02cbc26df43e0add65a34e
Caglan <cakpinar23@gmail.com>
2021-08-03 22:42:37 +0300

    number ıf logs are increased

--- customeranalytics/web/app/home/models.py
+++ customeranalytics/web/app/home/models.py
@@ -193,7 +193,7 @@ class RouterRequest:
             logs = self.collect_data_from_table(table='logs')
             if len(logs) != 0:
                 logs['color'] = logs['color'].apply(lambda x: 'color:' + x + ';')
-                self.message['logs'] = logs.to_dict('results')[-min(len(logs), 15):]
+                self.message['logs'] = logs.to_dict('results')[-min(len(logs), 40):]
             else:
                 self.message['logs'] = '....'
         except Exception as e: logging.error(e)
