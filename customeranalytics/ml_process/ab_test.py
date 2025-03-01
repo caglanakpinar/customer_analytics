5b989f4705ef0311c57ca09cfa8ce6699d6e67c8
caglanakpinar <cakpinar23@gmail.com>
2025-03-01 14:27:36 +0100

    Update ab_test.py

--- customeranalytics/ml_process/ab_test.py
+++ customeranalytics/ml_process/ab_test.py
@@ -1,35 +1,23 @@
 import sys, os, inspect
 import pandas as pd
 import numpy as np
+import datetime
 from itertools import product
-
 from ab_test_platform.executor import ABTest
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
-from customeranalytics.configs import default_es_port, default_es_host, time_periods
-from customeranalytics.utils import *
-from customeranalytics.data_storage_configurations.query_es import QueryES
+from customeranalytics.ml_process.base import BaseML
 from customeranalytics.ml_process.customer_segmentation import CustomerSegmentation
 
 
-class ABTests:
+class ABTests(BaseML):
     """
     AB Test;
     For more information pls check; https://pypi.org/project/abtest/
 
 
     """
-    def __init__(self,
-                 temporary_export_path,
-                 has_product_connection=True,
-                 has_promotion_connection=True,
-                 host=None,
-                 port=None,
-                 download_index='downloads',
-                 order_index='orders'):
+    def __init__(self, temporary_export_path, has_product_connection=True, has_promotion_connection=True, host=None,
+                 port=None, download_index='downloads', order_index='orders'):
         """
         ******* ******** *****
         Dimensional AB Test:
@@ -54,24 +42,22 @@ class ABTests:
         :param download_index: elasticsearch port
         :param order_index: elasticsearch port
         """
-        self.port = default_es_port if port is None else port
-        self.host = default_es_host if host is None else host
-        self.download_index = download_index
-        self.order_index = order_index
+        super().__init__(host, port, download_index, order_index)
         self.has_product_connection = has_product_connection
         self.has_promotion_connection = has_promotion_connection
         self.test = None
-        self.query_es = QueryES(port=port, host=host)
-        self.cs = CustomerSegmentation(port=self.port, host=self.host, order_index=self.order_index,
-                                       download_index=self.download_index)
+        self.cs = CustomerSegmentation(
+            port=self.port, host=self.host, order_index=self.order_index,
+            download_index=self.download_index
+        )
         self.data = pd.DataFrame()
         self.products = pd.DataFrame()
         self.promotions = []
         self.u_products = []
-        self.max_date = current_date_to_day()
+        self.max_date = self.current_date_to_day()
         self.dates = {}
         self.results = {}
-        self.time_periods = time_periods[1:]
+        self.time_periods = self.time_periods[1:]
         self.path = temporary_export_path
         self.features = ["orders", "amount"]
         self.fields = ["client", "session_start_date", "payment_amount", "id"]
@@ -84,32 +70,15 @@ class ABTests:
         self.promotion_combinations = []
         self.promotion_comparison = pd.DataFrame()
 
-    def dimensional_query(self, boolean_query=None):
-        if dimension_decision(self.order_index):
-            if boolean_query is None:
-                boolean_query = [{"term": {"dimension": self.order_index}}]
-            else:
-                boolean_query += [{"term": {"dimension": self.order_index}}]
-        return boolean_query
-
     def get_orders_data(self, end_date):
         """
         Purchased orders are collected from Orders Index.
         Session_start_date, client, payment_amount are needed for initializing CLv Prediciton.
         :param end_date: final date to start clv prediction
         """
-        self.query_es = QueryES(port=self.port, host=self.host)
         self.query_es.date_queries_builder({"session_start_date": {"lt": end_date}})
         self.query_es.query_builder(fields=self.fields, boolean_queries=self.dimensional_query())
         self.data = pd.DataFrame(self.query_es.get_data_from_es())
-
-    def dimensional_query(self, boolean_query=None):
-        if dimension_decision(self.order_index):
-            if boolean_query is None:
-                boolean_query = [{"term": {"dimension": self.order_index}}]
-            else:
-                boolean_query += [{"term": {"dimension": self.order_index}}]
-        return boolean_query
 
     def get_products(self, end_date):
         """
@@ -129,7 +98,6 @@ class ABTests:
         :param end_date: last date of data set
         """
         if self.has_product_connection:
-            self.query_es = QueryES(port=self.port, host=self.host)
             self.query_es.date_queries_builder({"session_start_date": {"lt": end_date}})
             self.query_es.query_builder(fields=None,
                                         boolean_queries=self.dimensional_query([{"term": {"actions.has_basket": True}}]),
@@ -157,7 +125,7 @@ class ABTests:
             # This will works to see how product of addition affects the total basket of payment amount
             self.products['payment_amount'] = self.products['payment_amount'].apply(lambda x: float(x))
             self.products['price'] = self.products['price'].apply(lambda x: float(x))
-            self.products['session_start_date'] = self.products['session_start_date'].apply(lambda x: convert_to_day(x))
+            self.products['session_start_date'] = self.products['session_start_date'].apply(self.convert_to_day)
 
     def get_customer_segments(self):
         """
@@ -165,18 +133,6 @@ class ABTests:
         :param date: given report date
         """
         self.data = pd.merge(self.data, self.cs.fetch()[['client', 'segments']], on='client', how='left')
-
-    def get_time_period(self, transactions, date_column):
-        """
-        converting date column of  values into the time_periods (hourly weekly, monthly,..)
-        :param transactions: total data (orders data with actions)
-        :return: data set with time periods
-        """
-        for p in list(zip(self.time_periods,
-                     [convert_dt_to_day_str, find_week_of_monday, convert_dt_to_month_str])):
-            transactions[p[0]] = transactions[date_column].apply(lambda x: p[1](x))
-        transactions[date_column] = transactions[date_column].apply(lambda x: convert_to_date(x))
-        return transactions
 
     def assign_organic_orders(self):
         """
@@ -239,7 +195,7 @@ class ABTests:
         :param date: current date
         """
         date = self.before_after_day_decision(date)
-        _day = convert_to_date(date)
+        _day = self.convert_to_date(date)
         _week = _day - datetime.timedelta(days=7)
         _month = _day - datetime.timedelta(days=30)
         _prev_day, _prev_week = [tp - datetime.timedelta(days=7) for tp in [_day, _week]]
@@ -512,104 +468,20 @@ class ABTests:
             del self.promotion_comparison
             self.promotion_comparison = None
 
-    def insert_into_reports_index(self,
-                                  abtest,
-                                  abtest_type,
-                                  index='orders'):
-        """
-        via query_es.py, each report can be inserted into the reports index with the given format.
-        {"id": unique report id,
-         "report_date": start_date or current date,
-         "report_name": "abtest",
-         "index": "main",
-         "report_types": {"abtest_type":  promotion_comparison || segments_change_monthly_before_after_amount, etc},
-         "data": abtest.fillna(0.0).to_dict("results") -  dataframe to list of dictionary
-         }
-         !!! null values are assigned to 0.
-
-        :param abtest: data set, data frame
-        :param start_date: data start date
-        :param abtest_type: orders, downloads, customer_journeys
-        :param index: dimensionality of data index orders_location1 ;  dimension = location1
-        """
-        list_of_obj = [{"id": np.random.randint(200000000),
-                        "report_date": convert_to_day(current_date_to_day()).isoformat(),
-                        "report_name": "abtest",
-                        "index": get_index_group(index),
-                        "report_types": {"abtest_type": abtest_type},
-                        "data": abtest.fillna(0.0).to_dict("results")}]
-        self.query_es.insert_data_to_index(list_of_obj, index='reports')
-
     def build_in_tests(self, date):
         """
         execute AB Test for given orders index
         :param date: recent date for query data
         :return:
         """
-        _date = str(current_date_to_day())[0:10] if date is None else date
+        _date = str(self.current_date_to_day())[0:10] if date is None else date
         self.get_orders_data(end_date=_date)
         self.get_products(end_date=_date)
         self.data = self.get_time_period(self.data, "session_start_date")
         self.assign_organic_orders()
         self.get_max_order_date()
         self.get_customer_segments()
-        self.generate_before_after_dates(convert_to_day(_date))
+        self.generate_before_after_dates(self.convert_to_day(_date))
         self.generate_test_groups()
         self.create_before_after_test(date)
         self.create_promotion_comparison_test(date)
-
-    def fetch(self, abtest_name, start_date=None):
-        """
-        Example of cohort_name;
-        promotion_comparison
-        segments_change_monthly_before_after_amount
-        segments_change_monthly_before_after_orders
-        segments_change_weekly_before_after_amount
-        segments_change_weekly_before_after_orders
-        segments_change_daily_before_after_amount
-        segments_change_daily_before_after_orders
-        product_usage_before_after_amount
-        product_usage_before_after_orders
-        promotion_usage_before_after_amount
-        promotion_usage_before_after_orders
-
-        Directly, these arguments are sent to elasticsearch reports index in order to fetch related reports.
-
-        Example AB Test Result;
-
-            	segments	accept_Ratio	mean_control	mean_validation	is_amount_increased_per_segments_per_monthly
-           0	lost	    0.633333	    24.180795	    23.996087	    False
-           1	at risk	    0.616667	    23.587849	    23.914558	    True
-           2	new customers	0.433333	31.431178	    31.835731	    True
-           3	champions	0.233333	    18.990791	    20.332927	    True
-           4	potential loyalist	0.133333	40.590746	39.738574	    False
-           5	can`t lose them	0.066667	24.448692	    16.543292	    False
-           6	need attention	0.016667	30.248727	    26.593158	    False
-           7	loyal customers	0.000000	28.847650	    29.915854	    True
-           8	others	   0.000000	        33.230453	    38.482046	    True
-           9	promising	0.000000	    42.453413	    24.186591	    False
-
-        :param abtest_name: e.g. promotion_comparison
-        :param start_date: directly sending start_date to report_date in reports index.
-        :return: data data-frame
-        """
-
-        boolean_queries, date_queries = [], []
-        boolean_queries = [{"term": {"report_name": "abtest"}},
-                           {"term": {"report_types.abtest_type": abtest_name}},
-                           {"term": {"index": get_index_group(self.order_index)}}]
-
-        if start_date is not None:
-            date_queries = [{"range": {"report_date": {"gte": convert_to_iso_format(start_date)}}}]
-
-        self.query_es = QueryES(port=self.port,
-                                host=self.host)
-        self.query_es.query_builder(fields=None, _source=True,
-                                    date_queries=date_queries,
-                                    boolean_queries=boolean_queries)
-        _res = self.query_es.get_data_from_es(index="reports")
-        _data = pd.DataFrame()
-        if len(_res) != 0:
-            _data = pd.DataFrame(_res[0]['_source']['data'])
-        return _data
-
