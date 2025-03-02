875c80be831d8ce7891f9cb53a0c11fc5cf3f6c1
caglanakpinar <cakpinar23@gmail.com>
2025-03-02 21:53:08 +0100

    removing sqlite connection 2

--- customeranalytics/data_storage_configurations/__init__.py
+++ customeranalytics/data_storage_configurations/__init__.py
@@ -2,13 +2,13 @@ from customeranalytics.data_storage_configurations.query_es import QueryES
 from customeranalytics.data_storage_configurations.data_access import GetData
 from customeranalytics.data_storage_configurations.es_create_index import CreateIndex
 from customeranalytics.data_storage_configurations.schedule_data_integration import Scheduler
-from customeranalytics.data_storage_configurations.sqlite import SQLiteDB
+from customeranalytics.data_storage_configurations.connection import Connection
 from customeranalytics.data_storage_configurations.storage import DataStorageConfigurations
 
 
 __all__ = [
     'DataStorageConfigurations',
-    'SQLiteDB',
+    'Connection',
     'Scheduler',
     'GetData',
     'QueryES'
