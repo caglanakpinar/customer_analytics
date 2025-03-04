d809df09112994083a216ee51906d09f6678d2f3
caglanakpinar <cakpinar23@gmail.com>
2025-03-04 12:21:10 +0100

    Update connection.py

--- customeranalytics/data_storage_configurations/connection.py
+++ customeranalytics/data_storage_configurations/connection.py
@@ -21,6 +21,9 @@ class BaseConnection:
 
 class Connection(Paths, Utils, Config):
     def __init__(self):
+        self.default_conf = BaseConnection.connection_config(
+            self.read_yaml(self.query_path, self.connection_file_name)
+        )
         self.connection_conf = BaseConnection.connection_config(
             self.read_yaml(self.query_path, self.connection_file_name)
         )
@@ -33,8 +36,8 @@ class Connection(Paths, Utils, Config):
         )
 
     def create_configuration(self, folder: str):
+        self.connection_folder = folder
         if not self.exists(folder, self.connection_file_name):
-            self.connection_folder = folder
             self.update_connection()
 
     def check_for_table_exits(self, table: str):
@@ -48,10 +51,17 @@ class Connection(Paths, Utils, Config):
                 return False
         return True
 
-    def update_logs(self, log: dict):
+    def add_logs(self, log: dict):
         self.connection_conf.logs.append(log)
         self.update_connection()
 
+    def update_actions(self, action: dict):
+        self.connection_conf.actions[action['data_type']] = action
+        self.update_connection()
+
+    def remove_action(self, data_type):
+        self.connection_conf.actions[data_type] = self.default_conf.actions[data_type]
+        self.update_connection()
 
     def collect_data_from_table(self, table='data_connection') -> dict:
         return getattr(self.connection_conf, table)
@@ -63,10 +73,6 @@ class Connection(Paths, Utils, Config):
             + [*self.collect_data_from_table('data_columns_integration').keys()]
             + [*self.collect_data_from_table('schedule_data').keys()]
         )
-
-        data_connection: dict = dict()
-        data_columns_integration: dict = dict()
-        schedule_data: dict = dict()
-        actions: list[dict] = []
+        return connection_keys
 
 
